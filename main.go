c51a59f3e18b0617b710d91ac2b7807529e91c9e
Kaffee <tobi.von.massow@gmail.com>
2022-03-18 17:27:12 +0100

    reading all packets now

--- main.go
+++ main.go
@@ -36,6 +36,14 @@ type OggHeader struct {
 	lacing_table             []byte
 }
 
+type Added_calc_data struct {
+	pcm_sample_position int
+	playback_time       int
+	total_body_length   int
+	number_of_packets   int
+	packet_lengths      []int
+}
+
 type OpusHead struct {
 	opus_head                      []byte
 	version                        []byte
@@ -62,13 +70,14 @@ func check(err error) {
 }
 
 var dec Decoder
+var totalPackets int = 0
 
-func readOggHeader() OggHeader {
+func readOggHeader(pre_skip []byte) (OggHeader, Added_calc_data) {
 	oggHeader := OggHeader{
 		capture_pattern:          dec.Read(4), // Ascii "OggS"
 		stream_structure_version: dec.Read(1), // Version must be 0
 		header_type:              dec.Read(1), // 0x01 Continuation | 0x02 BOS | 0x04 EOS
-		granule_position:         dec.Read(8), // Not needed for opus
+		granule_position:         dec.Read(8),
 		bitstream_serial_number:  dec.Read(4),
 		page_sequence_number:     dec.Read(4),
 		checksum:                 dec.Read(4),
@@ -76,7 +85,35 @@ func readOggHeader() OggHeader {
 	}
 
 	oggHeader.lacing_table = dec.Read(int(oggHeader.page_segments[0]))
-	return oggHeader
+	var added_calc_data Added_calc_data
+
+	if pre_skip != nil {
+		added_calc_data = Added_calc_data{
+			pcm_sample_position: int(binary.LittleEndian.Uint64(oggHeader.granule_position) - uint64(binary.LittleEndian.Uint16(pre_skip))),
+		}
+
+		added_calc_data.playback_time = added_calc_data.pcm_sample_position / 48000
+	}
+
+	i := 0
+	packetLengths := []int{}
+	for i < len(oggHeader.lacing_table) {
+		packetLength := 0
+		packetLength += int(oggHeader.lacing_table[i])
+		for oggHeader.lacing_table[i] == 255 {
+			i++
+			packetLength += int(oggHeader.lacing_table[i])
+		}
+		packetLengths = append(packetLengths, packetLength)
+		i++
+	}
+	added_calc_data.total_body_length = sum(packetLengths)
+	added_calc_data.number_of_packets = len(packetLengths)
+	added_calc_data.packet_lengths = packetLengths
+
+	totalPackets += added_calc_data.number_of_packets
+
+	return oggHeader, added_calc_data
 }
 
 func readOpusHead() OpusHead {
@@ -108,26 +145,36 @@ func readOpusHead() OpusHead {
 
 func readOpusTags() OpusTags {
 	/**
-  1) [vendor_length] = read an unsigned integer of 32 bits
-  2) [vendor_string] = read a UTF-8 vector as [vendor_length] octets
-  3) [user_comment_list_length] = read an unsigned integer of 32 bits
+	  1) [vendor_length] = read an unsigned integer of 32 bits
+	  2) [vendor_string] = read a UTF-8 vector as [vendor_length] octets
+	  3) [user_comment_list_length] = read an unsigned integer of 32 bits
 
-  4) iterate [user_comment_list_length] times {
-       5) [length] = read an unsigned integer of 32 bits
-       6) this iteration's user comment = read a UTF-8 vector as [length] octets
-     }
+	  4) iterate [user_comment_list_length] times {
+	       5) [length] = read an unsigned integer of 32 bits
+	       6) this iteration's user comment = read a UTF-8 vector as [length] octets
+			 }
 	*/
 	opusTags := OpusTags{}
 	opusTags.opus_tags = dec.Read(8)            // Ascii "OpusTags"
 	opusTags.vendor_string_length = dec.Read(4) // (always present) 4-byte little-endian length field, followed by length bytes of UTF-8 vendor string.
 	opusTags.vendor_string = dec.Read(int(binary.LittleEndian.Uint32(opusTags.vendor_string_length)))
-	opusTags.metadata_string_length = dec.Read(4)                                                          // 4-byte little-endian string count
+	opusTags.metadata_string_length = dec.Read(4) // 4-byte little-endian string count
 	n := binary.LittleEndian.Uint32(opusTags.metadata_string_length)
 	for i := uint32(0); i < n; i++ {
 		length := dec.Read(4)
 		opusTags.metadata_strings = append(opusTags.metadata_strings, dec.Read(int(binary.LittleEndian.Uint32(length)))...)
 	}
 	return opusTags
+}
+
+func readOpusPackets(data Added_calc_data) {
+	tocs := []byte{}
+	for _, length := range data.packet_lengths {
+		packet := dec.Read(length)
+		// decode to pcm whatever...
+		tocs = append(tocs, packet[0])
+	}
+	fmt.Println(tocs)
 }
 
 func main() {
@@ -139,15 +186,31 @@ func main() {
 		Buf:      f,
 	}
 
-	oggHeader1 := readOggHeader()
+	oggHeader1, _ := readOggHeader(nil)
 	opusHead := readOpusHead()
-	oggHeader2 := readOggHeader()
+	oggHeader2, _ := readOggHeader(nil)
 	opusTags := readOpusTags()
-	oggHeader3 := readOggHeader()
+	oggHeader3, addedData1 := readOggHeader(opusHead.pre_skip)
+	readOpusPackets(addedData1)
+	oggHeader4, addedData2 := readOggHeader(opusHead.pre_skip)
+	readOpusPackets(addedData2)
+
+	eos := false
+	for eos == false {
+		oggHeader, data := readOggHeader(opusHead.pre_skip)
+		if oggHeader.header_type[0] == 4 { // check for end of stream (EOS)
+			eos = true
+		} 
+		readOpusPackets(data)
+	}
 
 	fmt.Printf("%+v\n", oggHeader1)
 	fmt.Printf("%+v\n", opusHead)
 	fmt.Printf("%+v\n", oggHeader2)
 	fmt.Printf("%+v\n", opusTags)
 	fmt.Printf("%+v\n", oggHeader3)
+	fmt.Printf("%+v\n", addedData1)
+	fmt.Printf("%+v\n", oggHeader4)
+	fmt.Printf("%+v\n", addedData2)
+	fmt.Println(totalPackets)
 }
